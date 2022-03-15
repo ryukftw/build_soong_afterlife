70db36e56413a52eeae6ce10c74f8fce53b59634
Chenyang Zhong <zhongcy95@gmail.com>
2022-03-15 09:55:31 -0400

    PATH tools: allow python 3.10
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ia8b9b0427b8dc8c1d9382dc01784e224cdfd3af0
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -123,6 +123,7 @@ var Configuration = map[string]PathConfig{
         "python3.7":      Allowed,
         "python3.8":      Allowed,
         "python3.9":      Allowed,
+	"python3.10":     Allowed,
         "resize2fs":      Allowed,
         "repo":           Allowed,
 	"rsync":          Allowed,
