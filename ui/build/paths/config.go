b9dca53a0e56633483164b0dd71771c6c43f69ab
hmtheboy154 <buingoc67@gmail.com>
2022-12-22 14:01:35 -0500

    soong: Whitelist install
    For copy-firmware.sh script
    
    Change-Id: Ida5caf311796b105396d4bd041b353608a904933
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -105,6 +105,7 @@ var Configuration = map[string]PathConfig{
 	"git":            Allowed,
 	"grep":           Allowed,
 	"hexdump":        Allowed,
+	"install":        Allowed,
 	"jar":            Allowed,
 	"java":           Allowed,
 	"javap":          Allowed,
